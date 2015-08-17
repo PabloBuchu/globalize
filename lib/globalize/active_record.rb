3709d082b81ddbecc69801b7f2c4256098ae80e5
pawel@buchowski@gmail.com <pawel.buchowski@gmail.com>
2015-08-17 10:01:09 +0200

    add rest of dirty object methods

--- lib/globalize/active_record.rb
+++ lib/globalize/active_record.rb
@@ -13,9 +13,23 @@ module Globalize
     class << self
       def define_accessors(klass, attr_names)
         attr_names.each do |attr_name|
+          klass.send :define_method, attr_name, lambda { |*arg|
+                                     locale = arg.first
+                                     globalize.fetch locale || self.class.locale, attr_name
+                                   }
+          klass.send :define_method, "#{attr_name}=", lambda {|val|
+                                     attribute_will_change!(attr_name)
+                                     globalize.stash self.class.locale, attr_name, val
+                                   }
+          klass.send :define_method, "#{attr_name}_set", lambda {|val, locale|
+                                     globalize.stash locale || self.class.locale, attr_name, val
+                                   }
+          klass.send :define_method, "#{attr_name}_changed?", lambda {
+                                     attribute_changed? attr_name
+                                   }
           klass.send :define_method, "#{attr_name}_was", lambda {
-            changed_attributes[attr_name] if changed_attributes.include?(attr_name)
-          }
+                                     changed_attributes[attr_name] if changed_attributes.include?(attr_name)
+                                   }
         end
       end
     end
